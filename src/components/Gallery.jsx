c3203a8c0aa1b09836dfc773a33f3bccfaaad97e
felipefaundez7 <felipe.faundez7@gmail.com>
2023-10-02 03:20:13 -0300

    carga de vista Favorito, pero no renderiza photos con like, photos prop esta sigue undefined

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,46 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { useLikes } from '../context/LikesContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import Card from 'react-bootstrap/Card';
 import IconHeart from "./IconHeart";
 
 function Gallery({ photos }) {
   const { likes, toggleLike } = useLikes();
-  const [favorites, setFavorites] = useState([]);
   
   const handleToggleLike = (photoId) => {
     toggleLike(photoId); 
-
-    const updatedPhotos = photos.map((photo) => {
-      if (photo.id === photoId) {
-        return {
-          ...photo,
-          liked: !photo.liked,
-        };
-      }
-      return photo;
-    });
-
-    const selectedPhoto = updatedPhotos.find((photo) => photo.id === photoId);
-
-    if (selectedPhoto) {
-      if (selectedPhoto.liked) {
-        setFavorites([...favorites, selectedPhoto]);
-      } else {
-        const newFavorites = favorites.filter(
-          (photo) => photo.id !== selectedPhoto.id
-        );
-        setFavorites(newFavorites);
-      }
-    }
   };
-  console.log({photos })
+
+  console.log({ photos });
 
   return (
     <div className="container">
       <div className="row">
-        {photos.map((photo) => (
+        {photos && photos.map((photo) => (
           <div key={photo.id} className="col-12 col-sm-6 col-md-4 col-mb-4">
             <Card className="bg-dark text-white">
               <Card.Img variant="top" src={photo.src.tiny} alt={photo.alt} />
@@ -54,7 +30,7 @@ function Gallery({ photos }) {
                 <Card.Title>{photo.alt}</Card.Title>
                 <Card.Text>
                   {photo.photographer}
-                </Card.Text>  
+                </Card.Text>
               </Card.ImgOverlay>
             </Card>
           </div>
